ea7c10174f0d61a8cabe13313715d9a8f38498fb
euclidwindow <48415513+euclidwindow@users.noreply.github.com>
2023-07-08 06:49:34 -0700

    Create 01_contains_duplicate.py

---
+++ blind75/01_Arrays_and_hashing/01_contains_duplicate.py
@@ -0,0 +1,22 @@
+# check if there any duplicates in a given int array
+
+####################################################################
+# PYTHON3
+# Solution 1 = SORTING ; TC: O(n log n), SC: O(1)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums)):
+            if i+1 < len(nums) and nums[i] == nums[i+1]:
+                return True
+        return False
+
+# Solution 2 = USE SETS ; TC: O(n), SC : O(n)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for n in nums:
+            if n in seen:
+                return True
+            seen.add(n)
+        return False
