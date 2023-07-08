1881219c95ede2341ca41a0f04f9fd4b8eaac1ba
euclidwindow <48415513+euclidwindow@users.noreply.github.com>
2023-07-08 07:15:37 -0700

    Create 02_valid_anagram.py

---
+++ blind75/01_Arrays_and_hashing/02_valid_anagram.py
@@ -0,0 +1,16 @@
+# check if given two input words are anagrams of each other.
+
+# TC: O(n log n)
+# SC : O(n)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        s = ''.join(sorted(s))
+        t = ''.join(sorted(t))
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return False
+        return True
